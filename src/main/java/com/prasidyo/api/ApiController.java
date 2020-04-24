22921372981faf35a9ec9a9a89a7948efd93b0e2
prasidyo <prasidyo@gmail.com>
2020-04-24 14:46:51 +0700

    revisi

--- src/main/java/com/prasidyo/api/ApiController.java
+++ src/main/java/com/prasidyo/api/ApiController.java
@@ -1,11 +1,14 @@
 package com.prasidyo.api;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,45 +16,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
-	
-	@RequestMapping(value = "home", method = RequestMethod.GET)
-	public static String home() {
-		System.out.print("home console");
-		return "home api sukses";
-	}
-	
+
+	@CrossOrigin(origins = "http://localhost")
 	@ResponseBody
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ResponseEntity<String> getData() {
+	public ResponseEntity<String> index() {
 		String isi = "Saya adalah makelar kopi, tinggal di Laurier No. 37.";
-		
+		System.out.print(isi);
+
 		return new ResponseEntity<String>(isi, HttpStatus.OK);
 	}
 
-	//@CrossOrigin(origins = "http://localhost")
+	@CrossOrigin(origins = "http://localhost")
 	@ResponseBody
 	@RequestMapping(value = "/getData", method = RequestMethod.GET)
 	public ResponseEntity<Map<String, Object>> getData() {
 		Map<String, Object> map = new HashMap<String, Object>();
 		Map<String, String> detail = new HashMap<String, String>();
-		
+
 		detail.put("1", "satu");
 		detail.put("2", "dua");
 		detail.put("3", "tiga");
-		map.put("A", detail);
-		
-		detail = new HashMap<String, String>();
 		detail.put("4", "empat");
-		detail.put("5", "lima");
-		detail.put("6", "enam");
-		map.put("B", detail);
+		map.put("A", detail);
+
+		map.put("B", new ArrayList<String>(){{add("lima"); add("enam"); add("tujuh");}});
 		
-		detail = new HashMap<String, String>();
-		detail.put("7", "tujuh");
-		detail.put("8", "delapan");
-		detail.put("9", "sembilan");
-		map.put("B", detail);
-		
+		map.put("C", "delapan");
+		map.put("D", 9);
+		map.put("E", 10.000);
+		map.put("F", "");
+		map.put("G", null);
+
 		return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+	}
+
+	@CrossOrigin(origins = "http://localhost")
+	@ResponseBody
+	@RequestMapping(value = "/postData", method = RequestMethod.POST)
+	public ResponseEntity<Map<String, Object>> getPaymentVoucherDtl(@RequestBody Map<String, Object> input) {
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		try {
+			input.put("message", "sukses");
+			map.put("status", true);
+			map.put("data", input);
+			return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			map.put("data", e.getMessage());
+			map.put("status", false);
+			e.printStackTrace();
+			return new ResponseEntity<Map<String, Object>>(map, HttpStatus.BAD_REQUEST);
+		}
 	}
 }
